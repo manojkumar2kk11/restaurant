111979f8b65190a797d4d50c57391e1c22d3c38d
Manojkumar Ayyappan <manojkumar2kk11@gmail.com>
2025-01-22 19:27:14 +0000

    Add the booktable html file, function for the booktable, avaiable table and book sucees

--- dine_smart/myrestaurant/views.py
+++ dine_smart/myrestaurant/views.py
@@ -3,3 +3,42 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'home/home.html')
+
+def booktable(request):
+    return render(request, 'booktable/booktable.html', {'user': request.user})
+
+def book_table(request):
+    if request.method == 'POST':
+        form = ReserveForm(request.POST)
+        if form.is_valid():
+            booked_date = form.cleaned_data['date']
+            booked_time = form.cleaned_data['time']
+            guests = form.cleaned_data['guests']
+
+            booked_table = available_table(booked_date, booked_time, guests)
+
+            if booked_table:
+                reservation = form.save(commit=False)
+                reservation.user = request.user
+                reservation.table = booked_table
+                reservation.save()
+                return redirect('book_success')
+            else:
+                error_message = "There are no tables available for the chosen time and number of guests."
+                return render(request, 'booktable/booktable.html', {'form': form, 'error_message': error_message})
+    else:
+        form = ReserveForm()
+
+    return render(request, 'booktable/booktable.html', {'form': form})
+
+def available_table(booked_date, booked_time, guests):
+    available_tables = Table.objects.filter(table_guests__gte=guests)
+
+    for table in available_tables:
+        if not Reservation.objects.filter(table=table, date=booked_date, time=booked_time).exists():
+            return table
+
+    return None
+
+def book_success(request):
+    return render(request, 'successbooking/successbooking.html')
