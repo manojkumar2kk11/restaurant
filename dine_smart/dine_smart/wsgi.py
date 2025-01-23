e1fd1df12d2c4dcbd84cdb0c44595123044fb396
Manojkumar Ayyappan <manojkumar2kk11@gmail.com>
2025-01-23 22:02:53 +0000

    Fix the dir in setting , wsgi

--- dine_smart/dine_smart/wsgi.py
+++ dine_smart/dine_smart/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dine_smart.dine_smart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dine_smart.settings')
 
 application = get_wsgi_application()
