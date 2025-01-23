dc958ae73c5cd29b0ddbf731f261a26c85bde6cd
Manojkumar Ayyappan <manojkumar2kk11@gmail.com>
2025-01-23 22:33:46 +0000

    Fix the dir folder and the env file

--- dine_smart/dine_smart/settings.py
+++ dine_smart/dine_smart/settings.py
@@ -30,8 +30,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-TESTING= False
+DEBUG = True
+TESTING= True
 
 if DEBUG:
     ALLOWED_HOSTS = ['8000-manojkumar2k-restaurant-bs1rcczclmt.ws.codeinstitute-ide.net']
