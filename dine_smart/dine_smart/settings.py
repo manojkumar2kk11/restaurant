bc105df98e755cbca98073efbfed990298157337
Manojkumar Ayyappan <manojkumar2kk11@gmail.com>
2025-01-23 21:22:21 +0000

    Install the postgre database

--- dine_smart/dine_smart/settings.py
+++ dine_smart/dine_smart/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = ['8000-manojkumar2k-restaurant-bs1rcczclmt.ws.codeinstitute-ide.net']
